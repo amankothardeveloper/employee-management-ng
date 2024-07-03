9760a715500e9b9f997ed6ce909a67dbbcf235b7
= <=>
2024-07-03 18:36:34 +0530

    appwrite added in project

--- Employe-Management-System/src/app/app.component.ts
+++ Employe-Management-System/src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SvgComponent } from './svg/svg.component';
+import { ID, account } from '../lib/appwrite';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SvgComponent],
+  imports: [RouterOutlet, SvgComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Employe-Management-System';
+  loggedInUser: any = null;
+  email: string = '';
+  password: string = '';
+  name: string = '';
+
+  async login(email: string, password: string) {
+    await account.createEmailPasswordSession(email, password);
+    this.loggedInUser = await account.get();
+  }
+
+  async register(email: string, password: string, name: string) {
+    await account.create(ID.unique(), email, password, name);
+    this.login(email, password);
+  }
+
+  async logout() {
+    await account.deleteSession('current');
+    this.loggedInUser = null;
+  }
 }
